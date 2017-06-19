b96b36dabc975392f0cac7354f355783fc994765
Andrey Zac <az@stepcart.com>
2017-06-19 17:08:15 +0300

    fix

--- lib/GoMage/Linkedin/linkedinoauth.php
+++ lib/GoMage/Linkedin/linkedinoauth.php
@@ -3,10 +3,10 @@
  * GoMage Social Connector Extension
  *
  * @category     Extension
- * @copyright    Copyright (c) 2013-2015 GoMage (http://www.gomage.com)
+ * @copyright    Copyright (c) 2013-2017 GoMage (https://www.gomage.com)
  * @author       GoMage
- * @license      http://www.gomage.com/license-agreement/  Single domain license
- * @terms of use http://www.gomage.com/terms-of-use
+ * @license      https://www.gomage.com/license-agreement/  Single domain license
+ * @terms of use https://www.gomage.com/terms-of-use
  * @version      Release: 1.5.0
  * @since        Class available since Release 1.0
  */
